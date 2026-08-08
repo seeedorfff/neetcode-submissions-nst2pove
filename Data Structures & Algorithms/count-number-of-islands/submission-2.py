479197d919e27a429f942089c15355ab524a0a4a
Seedorf Otchere <105150335+seeedorfff@users.noreply.github.com>
2026-08-08 08:46:01 -0400

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        res = 0
+
+        def spread(r, c):
+            dxns = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+            for dr, dc in dxns:
+                nr, nc = r+dr, c+dc
+                
+                # conditions for spread (not to run)
+                if (nr < 0 or nr >= rows or
+                    nc < 0 or nc >= cols or
+                    grid[nr][nc] == "0"):
+                    continue
+                grid[nr][nc] = "0"
+                spread(nr, nc)
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    grid[r][c] = "0"
+                    spread(r, c)
+                    res += 1
+        return res
